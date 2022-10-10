daaab2b8c65ef36cde8785176eb4faa9471ec43b
David Martinez Gil <davidmartinezgilp@gmail.com>
2022-10-10 09:21:31 -0400

    Improved println statement and toString method

--- Horse.java
+++ Horse.java
@@ -1,1 +1,16 @@
-//Create the Horse class here.
+public class Horse extends Stable{
+    String name;
+    double weight;
+    boolean isTame;
+
+    public Horse(String n, double w, boolean tame) {
+        name = n;
+        weight = w;
+        isTame = tame;
+    }
+
+    //String name, double weight, boolean tame
+    public String toString(){
+        return ( "Name: "+this.name + " Weight: "+this.weight + " Tame: "+this.isTame);
+    }
+}
